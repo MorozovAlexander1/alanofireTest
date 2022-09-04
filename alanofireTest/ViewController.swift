c5053c1281a8bf148fe2b5e3a44a0881ef61c33b
Morozov <alexandermorozov19@yandex.ru>
2022-09-04 22:22:13 +0300

    commit test

--- alanofireTest/ViewController.swift
+++ alanofireTest/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageJson: UIImageView!
+    @IBOutlet weak var activityidentificator: UIActivityIndicatorView!
+    
+    var imageUrl: String = "https://www.ixbt.com/img/n1/news/2019/8/3/ALL-other-iPhone_0_large.jpg"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageJson.layer.cornerRadius = 20
+        activityidentificator.startAnimating()
+        downloadImage(url: imageUrl)
+        
     }
 
 
+    func downloadImage(url: String) {
+        
+        AF.download(url).responseData { (response) in
+            
+            if let data = response.value {
+                
+                let ImageData = UIImage(data: data)
+                
+                self.imageJson.image = ImageData
+                
+                self.activityidentificator.stopAnimating()
+                self.activityidentificator.isHidden = true
+                //self.activityidentificator.isHidden = true
+            }
+        }
+        
+    }
+    
 }
 
